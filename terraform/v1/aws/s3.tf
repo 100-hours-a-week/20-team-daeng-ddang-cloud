e7819cf640a7a84d3b7b1fb2fa763f7ca6e3c63b
kmi0817 <kmi0817@naver.com>
2026-01-25 20:02:59 +0900

    [feat] s3 코드 추가  (퍼블릭 액세스 허용)

---
+++ terraform/v1/aws/s3.tf
@@ -0,0 +1,47 @@
+resource "aws_s3_bucket" "daeng_map" {
+  bucket = var.project_name
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-${var.environment}-s3"
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "daeng_map" {
+  bucket = aws_s3_bucket.daeng_map.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "daeng_map" {
+  bucket = aws_s3_bucket.daeng_map.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+data "aws_iam_policy_document" "daeng_map_public_read" {
+  statement {
+    sid     = "PublicReadGetObject"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    resources = ["${aws_s3_bucket.daeng_map.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "daeng_map" {
+  bucket = aws_s3_bucket.daeng_map.id
+  policy = data.aws_iam_policy_document.daeng_map_public_read.json
+
+  # PublicAccessBlock 선적용 후 정책 적용 (적용 순서 안정화용)
+  depends_on = [aws_s3_bucket_public_access_block.daeng_map]
+}
