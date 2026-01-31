8639aae66c9962b4b16b7b360192b850200901b7
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-01-31 16:33:58 +0900

    feat : Staging 서버 CloudWatch 연동 추가

---
+++ terraform/v1/aws/staging_cloudwatch/outputs.tf
@@ -0,0 +1,29 @@
+output "monitored_instance_ids" {
+  description = "List of staging instance IDs being monitored"
+  value       = data.aws_instances.staging.ids
+}
+
+output "monitored_instance_count" {
+  description = "Number of staging instances being monitored"
+  value       = length(data.aws_instances.staging.ids)
+}
+
+output "dashboard_url" {
+  description = "CloudWatch Dashboard URL"
+  value       = length(data.aws_instances.staging.ids) > 0 ? "https://${var.region}.console.aws.amazon.com/cloudwatch/home?region=${var.region}#dashboards:name=staging-instances" : null
+}
+
+output "alarm_names" {
+  description = "List of created CloudWatch alarm names"
+  value = {
+    cpu_high              = [for alarm in aws_cloudwatch_metric_alarm.cpu_high : alarm.alarm_name]
+    memory_high           = [for alarm in aws_cloudwatch_metric_alarm.memory_high : alarm.alarm_name]
+    network_in_high       = [for alarm in aws_cloudwatch_metric_alarm.network_in_high : alarm.alarm_name]
+    network_out_high      = [for alarm in aws_cloudwatch_metric_alarm.network_out_high : alarm.alarm_name]
+    ebs_read_iops_high    = [for alarm in aws_cloudwatch_metric_alarm.ebs_read_iops_high : alarm.alarm_name]
+    ebs_write_iops_high   = [for alarm in aws_cloudwatch_metric_alarm.ebs_write_iops_high : alarm.alarm_name]
+    ebs_read_throughput   = [for alarm in aws_cloudwatch_metric_alarm.ebs_read_throughput_high : alarm.alarm_name]
+    ebs_write_throughput  = [for alarm in aws_cloudwatch_metric_alarm.ebs_write_throughput_high : alarm.alarm_name]
+    disk_used_high        = [for alarm in aws_cloudwatch_metric_alarm.disk_used_high : alarm.alarm_name]
+  }
+}
---
+++ terraform/v1/aws/staging_cloudwatch/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "environment" {
+  description = "Environment tag to filter instances"
+  type = string
+  default = "staging"
+}
+
